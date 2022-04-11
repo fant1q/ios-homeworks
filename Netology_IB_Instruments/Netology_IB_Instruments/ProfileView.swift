5666c269380843050ee556cd7be6c669caa35b48
fant1q <fant1kcs@yandex.ru>
2022-04-11 15:52:50 +0300

    создан ProfileView.swift и добавлены @IBOoutlet'ы для всех элементов

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Денис Штоколов on 11.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthDate: UILabel!
+    @IBOutlet weak var town: UILabel!
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var text: UITextView!
+    
+}
