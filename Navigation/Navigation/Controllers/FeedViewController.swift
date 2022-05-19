48a008b19e7b7a6efe1b667bfbfbe7fee6d208b0
fant1q <fant1kcs@yandex.ru>
2022-05-19 22:11:16 +0300

    adding UIStackView

--- Navigation/Navigation/Controllers/FeedViewController.swift
+++ Navigation/Navigation/Controllers/FeedViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
+    var buttonStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.backgroundColor = .systemGray
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        return stackView
+    }()
     var pushButton: UIButton = {
         let button = UIButton()
         button.setTitle("Go to the post", for: .normal)
         button.backgroundColor = .systemMint
         button.layer.cornerRadius = 15
-        button.frame = CGRect(x: 140, y: 200, width: 150, height: 70)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(handleButtonTap), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    var pushButton2: UIButton = {
+        let button = UIButton()
+        button.setTitle("Go to the post", for: .normal)
+        button.backgroundColor = .systemCyan
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitleColor(.blue, for: .normal)
         button.addTarget(self, action: #selector(handleButtonTap), for: .touchUpInside)
         
@@ -26,8 +48,20 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(pushButton)
         self.view.backgroundColor = .systemBackground
+        buttonStack.addArrangedSubview(pushButton)
+        buttonStack.addArrangedSubview(pushButton2)
+        view.addSubview(buttonStack)
+        
+        NSLayoutConstraint.activate([
+            buttonStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            buttonStack.widthAnchor.constraint(equalToConstant: 150),
+            buttonStack.heightAnchor.constraint(equalToConstant: 150),
+            pushButton.widthAnchor.constraint(equalToConstant: 120),
+            pushButton2.widthAnchor.constraint(equalToConstant: 120),
+        ])
+        
     }
     
     @objc
