7b782c7ceef1c0f8bdff36c0c7e501bbc3922f14
fant1q <fant1kcs@yandex.ru>
2022-08-25 17:16:03 +0300

    URL request

--- Navigation/Navigation/AppDelegate.swift
+++ Navigation/Navigation/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
+    let appConfiguration = AppConfiguration.allCases.randomElement()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let mainCoordinator = MainCoordinator()
         window?.rootViewController = mainCoordinator.startApplication()
         self.window?.makeKeyAndVisible()
+        NetworkService.request(for: appConfiguration!)
         
         return true
     }
